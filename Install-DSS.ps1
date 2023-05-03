fee7fbcdf722464da6867ad8c596fef753704403
nivmishra <94644519+nivmishra@users.noreply.github.com>
2023-05-03 15:05:41 -0400

    Add files via upload

---
+++ Install-DSS.ps1
@@ -0,0 +1,4 @@
+﻿<# Get-ItemProperty HKLM\SOFTWARE\WOW6432Node\New World Systems\Decision Support\* #>
+<# Get-ItemProperty HKEY_CLASSES_ROOT\Installer\Products\4BB0602F298E2584FBCB338F5770BBE9\* #>
+
+Install-Package C:\Users\Administrator\Desktop\NewWorld.DecisionSupport.msi
